2072edd3cc8cf8078f95f1c8fadd397e2f8c646c
ozzeau <anasschyzo@hotmail.com>
2025-10-26 20:59:38 +0100

    QOL#2

--- src/components/WelcomeCard.js
+++ src/components/WelcomeCard.js
@@ -4,8 +4,17 @@ import './CanvasBoard.css'; // Styles are shared in the main CSS file
 
 const WelcomeCard = ({ sign, onSign }) => (
   <div className="welcome-card-content">
-    <h2 style={{ marginTop: 0 }}>Welcome to the Canvas</h2>
-    <p>Pan around to see other signatures or switch to Draw mode to add your own.</p>
+    <h2 style={{ marginTop: 0 }}>Hey! Sign my page!</h2>
+    <p>
+      Sign my page your way by clicking the button below.
+      <br />
+      Cooldown has not been implemented as of yet, so this is operating on an honor based system so chill out.  
+      <br />
+      <br />
+      
+      <a href="https://github.com/ozzeau/simypa" target="_blank" rel="noopener noreferrer"style={{ color: '#FFA500', textDecoration: 'underline' }}>How </a>it works
+
+    </p>
     <CButton label={sign} onClick={onSign} />
   </div>
 );
